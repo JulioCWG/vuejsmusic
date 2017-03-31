5e65bdcc1a76ce30b0acf2a2b4e84aacb38e6e79
Julio Cesar <castro_jc88@hotmail.com>
2017-03-31 13:14:40 -0700

    Comit inicial

---
+++ src/api/config.js
@@ -0,0 +1,24 @@
+
+export default {
+  appName:'platzimusic',
+  apiKey:'697c3889e749bc8b1ef709488b5982da',
+  secret:'31ea568d58a8bd41b9ee2cf52136fd47',
+  registeredTo:'juliofm10'
+}
+
+//
+//
+// Here are the details of your new API account.
+//
+//
+// Application name
+// platzimusic
+//
+// API key
+// 697c3889e749bc8b1ef709488b5982da
+//
+// Shared secret
+// 31ea568d58a8bd41b9ee2cf52136fd47
+//
+// Registered to
+// juliofm10
